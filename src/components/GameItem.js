e582e00435d57aa2a2c34aaae0d0e9a418934947
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-25 15:30:25 -0800

    wave 2

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,27 +4,50 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor() {
+    super();
+    this.state ={
+      spotted: false,
+    };
+  }
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
 
   onItemClicked = () => {
-    // Fill this in for Wave 2!
+    this.setState({ spotted: true });
   }
-    
-  render() {
+
+  
+render() {
+  let styleName = "";
+  if ((this.props.type === "litter") &&  (this.state.spotted === true )) {
+    styleName = "game-item spotted-litter";
+  }
+  else if ((this.props.type !== "litter") && (this.state.spotted === true )) {
+    styleName = "game-item spotted-nature";
+  }
+  else if (this.state.spotted === false ) {
+    styleName = "game-item";
+  }
+ 
+
+
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+      
     };
 
     // Update this to select the correct icon for each item
     const icon = ItemIcons[this.props.type];
 
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className={styleName} style={itemStyle}>
+          <img src={icon} alt="Item" className="icon-item" onClick= { this.onItemClicked }></img>
+        
       </div>
     );
   }
