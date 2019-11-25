0856f46791fa6e195be89bee0da8c9d6a9f58cf3
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-25 14:48:48 -0800

    added classes to display check or x on click

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,6 +4,13 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: "game-item",
+    }
+  }
+  
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
@@ -11,6 +18,11 @@ class GameItem extends Component {
 
   onItemClicked = () => {
     // Fill this in for Wave 2!
+    if (this.props.itemType === 'litter') {
+      this.setState( { classes: this.state.classes.concat(" spotted-litter") } );
+    } else {
+      this.setState( { classes: this.state.classes.concat(" spotted-nature") } );
+    }
   }
     
   render() {
@@ -25,7 +37,7 @@ class GameItem extends Component {
     const icon = ItemIcons[itemIcon];
 
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className={this.state.classes} style={itemStyle} onClick={ this.onItemClicked }>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );
