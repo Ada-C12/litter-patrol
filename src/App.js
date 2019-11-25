d7d162ba371531cb488ceaf7aa443a53488c5dd5
KKennedyCodes <holmstedtk@gmail.com>
2019-11-25 15:47:33 -0800

    Waves 1 and 2 Complete

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ class App extends Component {
     };
 
     // Uncomment this to spawn a single test item
-    // const testItem = this.spawnItem(Date.now());
-    // this.state.items.push(testItem);
+    const testItem = this.spawnItem(Date.now());
+    this.state.items.push(testItem);
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
@@ -49,7 +49,7 @@ class App extends Component {
                height={item.height}     // Height - used for a CSS style to position on the screen
                layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
                key={item.id}            // Key - to help React with performance
-
+              type={item.type}
                // Additional props (event callbacks, etc.) can be passed here
              />;
     });
--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,27 +4,48 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false,
+      cssClass: 'game-item',
+    };
+  }
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
-
-  onItemClicked = () => {
+  
+  onItemClicked = (props) => {
     // Fill this in for Wave 2!
+    let icon = this.props.type;
+    if (icon==='litter') {
+      this.setState({
+        cssClass: 'game-item spotted-litter',
+      })
+    } else {
+      this.setState({
+        cssClass: 'game-item spotted-nature',
+      })
+    }
+    return;
   }
     
   render() {
+    
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    let imageHelper = this.props.type;
+    const icon = ItemIcons[imageHelper];
 
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className={this.state.cssClass} style={itemStyle}>
+        <img src={icon} alt="Item" className='icon-item' onClick={this.onItemClicked} ></img>
       </div>
     );
   }
