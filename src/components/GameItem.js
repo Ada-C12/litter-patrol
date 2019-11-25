5a1afb5ce45ad5ecb1fd88543077b912481c9ce9
Janice Huang <janicehuang@gmail.com>
2019-11-25 14:45:17 -0800

    adapt code so icons rendered according to gameItem type

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -20,10 +20,10 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    const icon = 'ItemIcons.' + ${this.props.type};
+      let icon = ItemIcons[this.props.type]
 
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className={`game-item ${this.className}`} style={itemStyle}>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );
