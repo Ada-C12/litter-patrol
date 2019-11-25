8c11a5f99c07056915432e0a927e529f1fb97612
eaball35 <eaball35@gmail.com>
2019-11-25 15:53:49 -0800

    icons & onclick

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,27 +4,43 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false,
+    };
+  }
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
-
+  
   onItemClicked = () => {
-    // Fill this in for Wave 2!
+    this.setState({ clicked: !this.state.clicked });
   }
-    
+
+  
   render() {
+    console.log(this.state)
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type]
 
+    let spotClass = ''
+    if (this.state.clicked === true) {
+      spotClass = 'spotted-nature'
+      if (this.props.type === 'litter') {
+        spotClass = 'spotted-litter'
+      } 
+    }
+    
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div onClick={this.onItemClicked} className={`game-item ${spotClass}`} style={itemStyle}>
+        <img src={icon} alt="Item" className='icon-item'></img>
       </div>
     );
   }
