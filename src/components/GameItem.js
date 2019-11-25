0505171347def2e3e9785416c6d4c4376407a4a7
brilatimer <brihiramine@gmail.com>
2019-11-25 15:04:27 -0800

    baseline and wave 1 complete - litter and nature working

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,14 +4,37 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+      constructor(event){
+        super()
+        this.state = {
+          trash: false
+        };
+      }
+    
+        // if item has litter, green
+          // else item has no litter, red
+          // use event handler to 
+          // item: 'spotted-litter',
+          // item: 'spotted-nature'
+
+        // state === false 
+        // state === true 
+
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
 
   onItemClicked = () => {
+    this.setState({trash: !this.state.trash})
+    // if this.state === 'spotted-litter'
+    // elsif this.state === 'spotted-nature'
+    
     // Fill this in for Wave 2!
   }
+
+  
     
   render() {
     const itemStyle = {
@@ -20,7 +43,7 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type];
 
     return (
       <div className="game-item" style={itemStyle}>
