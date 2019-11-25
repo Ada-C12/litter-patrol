035248a28f44e05eff70b658753db5792e1a2570
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-11-25 13:57:48 -0800

    Updated GameItem to choose random itemicon

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,6 +4,16 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor () {
+    super()
+
+    const pickIcon = () => {
+      var keys = Object.keys(ItemIcons)
+      return ItemIcons[keys[ keys.length * Math.random() << 0]];
+    }
+    this.icon = pickIcon()
+  }
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
@@ -12,19 +22,16 @@ class GameItem extends Component {
   onItemClicked = () => {
     // Fill this in for Wave 2!
   }
-    
+
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
-
     return (
       <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+        <img src={this.icon} alt="Item" className="icon-item"></img>
       </div>
     );
   }
