691323f9c16797af7a6314561211c3579848e1cd
Alice <alicesunhi@gmail.com>
2019-11-25 14:34:12 -0800

    different items are appearing

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import '../App.css';
-import ItemIcons from '../ItemIcons.js';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import "../App.css";
+import ItemIcons from "../ItemIcons.js";
+import PropTypes from "prop-types";
 
 class GameItem extends Component {
   static propTypes = {
     height: PropTypes.number.isRequired,
-    layer: PropTypes.number.isRequired,
-  }
+    layer: PropTypes.number.isRequired
+  };
 
   onItemClicked = () => {
     // Fill this in for Wave 2!
-  }
-    
+  };
+
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
-      zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+      zIndex: this.props.layer // use props.layer to set z-index, so we display ontop of background
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type];
 
     return (
       <div className="game-item" style={itemStyle}>
