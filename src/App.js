82c6a47670e143f15a8829029e0fbd0b5a862fd0
Yasmin <yasmin312002@hotmail.com>
2019-11-25 18:54:41 -0800

    added Spotting Items logic

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       flower: 5,
       mushroom: 5,
     },
-    spawnRate: 1.2, // Hz
+    spawnRate: .4, // Hz
     spawnRateRnd: 1.79, // randomization factor
     spawnHeight: 100, // height of item spawn area in pixels
     spawnFloor: 0, // offset from bottom of game "level" in pixels
@@ -49,7 +49,8 @@ class App extends Component {
                height={item.height}     // Height - used for a CSS style to position on the screen
                layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
                key={item.id}            // Key - to help React with performance
-               itemType={item.type}
+               type={item.type}
+               onItemClicked ={this.onItemClicked}
 
                // Additional props (event callbacks, etc.) can be passed here
                
--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,26 +4,47 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  constructor(props){
+    super(props);
+    this.state = ({
+     spotted: false,
+    });
+  }
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired
+   
   }
 
   onItemClicked = () => {
     // Fill this in for Wave 2!
+    this.setState({
+      spotted :true,
+    });
+
   }
     
   render() {
+
+
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons[this.props.itemType];
+    const icon = ItemIcons[this.props.type]
 
+    let classNames = 'game-item';
+    if (this.state.spotted) {
+      const style = this.props.type === 'litter' ? 'spotted-litter' : 'spotted-nature';
+      classNames = classNames + ' ' + style
+    }
+    
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className={classNames} style={itemStyle} onClick={this.onItemClicked}>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );
