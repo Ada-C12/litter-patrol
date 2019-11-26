182621915ab6f26cba88d0f87f9fd656e9fdb380
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-26 13:34:42 -0800

    added functions to render item types, spot items and score points

--- src/App.js
+++ src/App.js
@@ -30,28 +30,29 @@ class App extends Component {
     };
 
     // Uncomment this to spawn a single test item
-    // const testItem = this.spawnItem(Date.now());
-    // this.state.items.push(testItem);
+    const testItem = this.spawnItem(Date.now());
+    this.state.items.push(testItem);
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
-
     console.log(this.state);
   }
 
   onItemClicked = () => {
     // Fill this in for Wave 3!
+    this.setState({ points: this.state.points + 1 })
   }
 
   render() {
     const items = this.state.items.map((item, i) => {
       return <GameItem
-               height={item.height}     // Height - used for a CSS style to position on the screen
-               layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
-               key={item.id}            // Key - to help React with performance
-
+              height={item.height}     // Height - used for a CSS style to position on the screen
+              layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
+              key={item.id}            // Key - to help React with performance
+              type={item.type} 
+              onItemClicked={this.onItemClicked}
                // Additional props (event callbacks, etc.) can be passed here
-             />;
+            />;
     });
 
     return (
