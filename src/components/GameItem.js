55a99c1e009a5dcd3e11bc823a240a16539e2bf9
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-26 15:05:13 -0800

    Changed icon constant to take in different types of icons

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -38,10 +38,11 @@ class GameItem extends Component {
 
     // Update this to select the correct icon for each item
     // const icon = ItemIcons.rock;
-      
+    const icon = ItemIcons[this.props.type];
+
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className={`game-item ${this.className}`} style={itemStyle}>
+        <img src={icon} alt="Item" className="icon-item" onClick={this.onItemClicked}></img>
       </div>
     );
   }
