70383b46f42dcc9e45c21a0ebc2b7191d02374ca
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-11-26 17:25:33 -0800

    completed

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import '../App.css';
 import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
+import { spawn } from 'child_process';
+const randomObjProp = require('random-obj-prop');
+
 
 class GameItem extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      classForX: "game-item",
+      alreadyScored: false,
+    }
+  }
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
@@ -11,6 +22,22 @@ class GameItem extends Component {
 
   onItemClicked = () => {
     // Fill this in for Wave 2!
+   if (this.props.type === "litter"){
+      this.setState({
+      classForX: "game-item spotted-litter"
+      })
+      if (this.state.alreadyScored === false){
+        this.props.onClickCallback()
+        this.setState({
+          alreadyScored: true
+        })
+      }
+      
+   } else {
+       this.setState({
+        classForX: "game-item spotted-nature"
+       })
+    }
   }
     
   render() {
@@ -19,13 +46,20 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
 
+    // If we're spotted, add the appropriate class
+
+
+   
+    // Update this to select the correct icon for each item
+       const icon = ItemIcons[this.props.type];
+
+    
     return (
-      <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+      <div className= {this.state.classForX} style={itemStyle}>
+        <img src={icon} alt="Item" className="icon-item" onClick = {this.onItemClicked}></img>
       </div>
+      
     );
   }
 }
