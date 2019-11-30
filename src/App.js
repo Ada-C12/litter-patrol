046741a4ae2dda1c09abbc1d24ee058943cbefc4
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-11-30 15:42:53 -0800

    render single item

--- src/App.js
+++ src/App.js
@@ -29,13 +29,14 @@ class App extends Component {
       points: 0,
     };
 
-    // Uncomment this to spawn a single test item
+    // // Uncomment this to spawn a single test item
     // const testItem = this.spawnItem(Date.now());
     // this.state.items.push(testItem);
+    // console.log(this.state);
+
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
-
     console.log(this.state);
   }
 
@@ -49,8 +50,10 @@ class App extends Component {
                height={item.height}     // Height - used for a CSS style to position on the screen
                layer={100 + i}          // Layer - used for a CSS style to show items on-top of bg
                key={item.id}            // Key - to help React with performance
+               
 
                // Additional props (event callbacks, etc.) can be passed here
+               itemType = {item.type}
              />;
     });
 
--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,66 +4,36 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+
   static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
 
-  iconChooser = () => {
-
-  //THIS SECTION DETERMINES HOW THE ICON WILL BE CHOSEN:
-    // Note, handles are shuffled before being put in object because object cannot be shuffled. ATM, wondering if this is better done with a map. Are the choices really random or are they running 1-5 really fast? 
-
-    // an array that will be used as identifications for handles to grab specific icons
-    const choices = [1, 2, 3, 4, 5]
-
-    // shuffle function
-    const shuffleIcons = function (array) {
-      for (var i = array.length - 1; i > 0; i--) {
-          var j = Math.floor(Math.random() * (i + 1));
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      };
-    };
-
-    // call shuffle function on handles
-    shuffleIcons(choices)
-
-    //attach handles to specific elements
-    const really = {
-      1: ItemIcons.litter,
-      2: ItemIcons.rock,
-      3: ItemIcons.bush,
-      4: ItemIcons.flower,
-      5: ItemIcons.mushroom
+    constructor (request, _props) {
+      super ()
+      this.request = request
     }
 
-    return really[choices[0]]
-
-  }
-
   render() {
-
 
   // THIS SECTION DETERMINES HOW/WHERE THE ICON WILL BE DISPLAYED
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
+    
+    //itemType is actually a reference to App.js...which is how GameItem knows which item it's actually trying to render. 
 
-    // Update this to select the correct icon for each item
-    // const icon = ItemIcons.rock;
-
-  
-    // this selects the item from really based on whatever is at the first handle spot
-    let icon = this.iconChooser()
+    const icon = ItemIcons[this.props.itemType]
 
     return (
       
       <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" 
-        className="icon-item"></img>
+        <img src={icon} alt="item" className="icon-item"></img>
+      
+      
+      
       </div>
     );
   }
